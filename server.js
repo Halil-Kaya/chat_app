fd2af497692f4765fe79ce9f21ccd9873c26eb57
halil-kaya <hibrahimkayahik@gmail.com>
2021-05-25 15:48:13 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const http = require('http')
+const { IPv4 } = require('ipaddr.js')
+const path = require('path')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+
+io.on('connection',(socket) => {
+
+    console.log('New WS Connection...')
+
+    //tek bir cliente
+    socket.emit('message','Welcome to ChatCord!')
+
+    //yeni bagli client disindaki butun clientlere
+    socket.broadcast.emit('message','A user has joined the chat')
+
+    //butun clientlere
+    //io.emit()
+
+    //client disconnect oldugunda
+    socket.on('disconnect',() => {
+
+        //butun clientlere kullanicinin ayrildigi mesajını veriyor
+        io.emit('message','A user has left the chat')
+
+    })
+
+})
+
+
+const PORT = 3000 || process.env.PORT;
+
+
+server.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`)
+})
